96bc909eb8375acd6005955900a0e424948ccadd
Fier <alan.sanchezram@alumno.buap.mx>
2026-02-13 00:44:32 -0600

    Creación de la base de datos y funcion de consulta desde db_query.py

---
+++ backend/db_query.py
@@ -0,0 +1,34 @@
+from sqlalchemy import create_engine, text
+import pandas as pd
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def autocompletado(palabra):
+
+    palabra = palabra.strip()
+
+    if len(palabra) > 50:
+        raise ValueError("Texto demasiado largo")
+
+    engine = create_engine(
+    os.getenv("DATABASE_URL")
+    )
+
+    query = text("""
+        SELECT descripcion
+        FROM fracciones_arancelarias
+        WHERE descripcion ILIKE :busqueda
+        LIMIT 20
+    """)
+
+    with engine.connect() as conn:
+        df = pd.read_sql(query, conn, params={
+            "busqueda": f"%{palabra}%"
+        })
+
+    return df["descripcion"].drop_duplicates().tolist()
+
+#lista = autocompletado("Maíz")
+#print(lista)
